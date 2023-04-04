6b2bc15d86c4f22878bdff97ee806862dcab0e14
Sean Murphy <smurphspam005@gmail.com>
2023-04-04 17:11:11 +1000

    Add scrollbar

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,11 +1,13 @@
-import _FlaskIcon from "./flask-icon.png";
-import _SqliteIcon from "./sqlite-icon.png";
-import _DjangoIcon from "./django-icon.png";
-import _PostgresqlIcon from "./postgresql-icon.png";
-import _FlutterIcon from "./flutter-icon.png";
+import _FlaskIcon from "./flask.png";
+import _SqliteIcon from "./sqlite.png";
+import _DjangoIcon from "./django.png";
+import _PostgresqlIcon from "./postgresql.png";
+import _FlutterIcon from "./flutter.png";
+import _WordpressIcon from "./wordpress.png";
 
 export const FlaskIcon = _FlaskIcon;
 export const SqliteIcon = _SqliteIcon;
 export const DjangoIcon = _DjangoIcon;
 export const PostgresqlIcon = _PostgresqlIcon;
 export const FlutterIcon = _FlutterIcon;
+export const WordPressIcon = _WordpressIcon;
