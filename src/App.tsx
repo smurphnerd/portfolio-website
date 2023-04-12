a3c13e9925a2d861b045eba1f58e0be824d782d8
Sean Murphy <smurphspam005@gmail.com>
2023-04-12 17:58:30 +1000

    Add image preloading

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
 import { RouterProvider } from "react-router-dom";
 import "./styles/styles.scss";
 import { router } from "./routes";
-import { CSSTransition } from "react-transition-group";
 import { useEffect, useState } from "react";
+import * as icons from "./assets/icons";
+import * as thumbnails from "./assets/thumbnails";
+import clouds from "./assets/clouds.png";
+import twinking from "./assets/twinkling.png";
+import stars from "./assets/stars.png";
+import smBackground from "./assets/sm-background.png";
+import smLogo from "./assets/sm-logo.svg";
 
 function App() {
-  // const [isLoading, setisLoading] = useState(true);
-  // console.log(isLoading);
+  type ImagePromise = Promise<Event>;
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setisLoading(false);
-  //   }, 1000);
-  // }, []);
+  // Creates a new promise that resolves on image onload
+  const loadImage = (src: string): ImagePromise => {
+    return new Promise((resolve) => {
+      const img = new Image();
+      img.onload = resolve;
+      img.src = src;
+    });
+  };
+
+  const loadAssets = async (): Promise<void> => {
+    const promises: ImagePromise[] = [
+      ...Object.values(icons).map(loadImage),
+      ...Object.values(thumbnails).map(loadImage),
+      loadImage(clouds),
+      loadImage(twinking),
+      loadImage(stars),
+      loadImage(smBackground),
+      loadImage(smLogo),
+    ];
+    await Promise.all(promises);
+  };
+
+  useEffect(() => {
+    loadAssets();
+  }, []);
 
   return <RouterProvider router={router} />;
 }
