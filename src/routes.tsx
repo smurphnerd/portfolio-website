c041f2b5aeddbde59e76c75b4bd0bfd8a20db6c3
Sean Murphy <smurph005@gmail.com>
2024-05-17 16:14:29 +1000

    fix: add to routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
 } from "react-router-dom";
 import Blog from "./pages/BlogHome";
 import ButterflyEffect from "./pages/projects/ButterflyEffect";
@@ -14,28 +14,30 @@ import ILearn from "./pages/projects/ILearn";
 import Vault from "./pages/projects/Vault";
 import { ArticleLayout } from "./pages/ArticleLayout";
 import B from "./pages/blogs";
+import Vodalogic from "./pages/projects/Vodalogic";
 
 export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Root />}>
-      <Route path="/" element={<MainTemplate />}>
-        <Route path="/" element={<Landing />} />
-        <Route path="projects" element={<Projects />} />
-        <Route path="inspiration" element={<Inspiration />} />
-        <Route path="blog" element={<Blog />} />
-      </Route>
-      <Route path="projects" element={<ArticleLayout link="/projects" />}>
-        <Route path="butterfly-effect" element={<ButterflyEffect />} />
-        <Route path="ilearn" element={<ILearn />}></Route>
-        <Route path="vault" element={<Vault />}></Route>
-      </Route>
-      <Route path="blog" element={<ArticleLayout link="/blog" />}>
-        <Route
-          path="why-i-started-programming"
-          element={<B.WhyIStartedProgramming />}
-        />
-        <Route path="the-butterfly-effect" element={<B.TheButterflyEffect />} />
-      </Route>
-    </Route>,
-  ),
+    createRoutesFromElements(
+        <Route path="/" element={<Root />}>
+            <Route path="/" element={<MainTemplate />}>
+                <Route path="/" element={<Landing />} />
+                <Route path="projects" element={<Projects />} />
+                <Route path="inspiration" element={<Inspiration />} />
+                <Route path="blog" element={<Blog />} />
+            </Route>
+            <Route path="projects" element={<ArticleLayout link="/projects" />}>
+                <Route path="butterfly-effect" element={<ButterflyEffect />} />
+                <Route path="ilearn" element={<ILearn />}></Route>
+                <Route path="vault" element={<Vault />}></Route>
+                <Route path="vodalogic" element={<Vodalogic />}></Route>
+            </Route>
+            <Route path="blog" element={<ArticleLayout link="/blog" />}>
+                <Route
+                    path="why-i-started-programming"
+                    element={<B.WhyIStartedProgramming />}
+                />
+                <Route path="the-butterfly-effect" element={<B.TheButterflyEffect />} />
+            </Route>
+        </Route>,
+    ),
 );
