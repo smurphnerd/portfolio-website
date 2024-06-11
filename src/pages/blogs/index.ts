eecd14453bdae66f6f3e30a6bfab55324a9e448a
Sean Murphy <smurph005@gmail.com>
2024-06-11 17:54:51 +1000

    feat: Add ai alignment blog

--- src/pages/blogs/index.ts
+++ src/pages/blogs/index.ts
@@ -1,20 +1,22 @@
 import { WhyIStartedProgramming } from "./why-i-started-programming/WhyIStartedProgramming";
 import { TheButterflyEffect } from "./the-butterfly-effect/TheButterflyEffect";
 import { LearningMoreByDoingLess } from "./learning-more-by-doing-less/LearningMoreByDoingLess";
+import { WhyImScaredAboutTheFutureOfAI } from "./why-im-scared-about-the-future-of-ai/WhyImScaredAboutTheFutureOfAI";
 
 export type BlogPost = {
-  title: string;
-  date: string;
-  thumbnail: string;
-  blurb: string;
-  route: string;
-  content: JSX.Element;
+    title: string;
+    date: string;
+    thumbnail: string;
+    blurb: string;
+    route: string;
+    content: JSX.Element;
 };
 
 export const BlogPosts: BlogPost[] = [
-  WhyIStartedProgramming,
-  TheButterflyEffect,
-  LearningMoreByDoingLess,
+    WhyIStartedProgramming,
+    TheButterflyEffect,
+    LearningMoreByDoingLess,
+    WhyImScaredAboutTheFutureOfAI,
 ];
 
 export default BlogPosts;
